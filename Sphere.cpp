586c87753e1d74b2cbf81213fc9c5ab9c59d98dc
jetSett <joel.felderhoff@ens-lyon.fr>
2016-12-06 14:38:10 +0100

    Adding sphere intersection

--- Sphere.cpp
+++ Sphere.cpp
@@ -1,9 +1,39 @@
 #include "Sphere.hpp"
 
 Sphere::Sphere(Point center, Scalar radius) : _center(center), _radius(radius){
-    
+
 }
 
-Point Sphere::intersect(const Ray&) const{
-    /// \todo la coder
+bool Sphere::intersect(const Ray& ray, Point& out) const{
+    // the equation to solve is : f(t) = (t**2)*(||d||)**2 -2*t*<d, OA> + ||OA||**2 - R**2 = 0
+    Scalar scalarProduct = ray.direction.dot(fromTo(ray.origin, _center));
+    Scalar squaredNormD = ray.direction.squaredNorm();
+    Scalar reduced_discrim = (scalarProduct*scalarProduct - squaredNormD*(  fromTo(ray.origin, _center).squaredNorm() - _radius*_radius   ) );
+    if(reduced_discrim < 0){
+      return false;
+    }else if(reduced_discrim == 0){
+      Scalar t = scalarProduct/squaredNormD;
+      out = ray.getPoint(t);
+      return true;
+    }else{
+      Scalar t1 = (scalarProduct + sqrt(reduced_discrim))/squaredNormD, t2 = (scalarProduct - sqrt(reduced_discrim))/squaredNormD;
+      if(t1 < 0 and t2 < 0){
+        return false;
+      }
+      if(t1 < 0 and t2 >= 0){
+        out = ray.getPoint(t2);
+        return true;
+      }
+      if(t1 >= 0 and t2  < 0){
+        out = ray.getPoint(t1);
+        return true;
+      }
+      //here, we are sure that t1 >=0 and t2 >=0
+      if(t1 > t2){
+        out = ray.getPoint(t2);
+      }else{
+        out = ray.getPoint(t1);
+      }
+      return true;
+    }
 }
--- Sphere.hpp
+++ Sphere.hpp
@@ -5,20 +5,20 @@
 #include "Primitives.hpp"
 
 class Sphere : public IShape {
-    
+
 public:
 
     Sphere(Point, Scalar);
-    
+
     /// \fn intersect
-    /// \brief get the intersection point with the Ray
-    virtual Point intersect(const Ray&) const override;
-    
+    /// \brief get the intersection point with ray
+    virtual bool intersect(const Ray& ray, Point& out) const override;
+
 private:
 
     Point _center;
     Scalar _radius;
-    
+
 };
 
 #endif
