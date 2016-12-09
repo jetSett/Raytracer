af9b2b9933c4ca52cb45863d4f5b5adbbcc0580d
Algue-Rythme <algue-rythme@orange.fr>
2016-12-09 10:50:44 +0100

    Correct typo and refactoring.

--- Sphere.cpp
+++ Sphere.cpp
@@ -1,15 +1,13 @@
 #include "Sphere.hpp"
 
-Sphere::Sphere(Point center, Scalar radius) : _center(center), _radius(radius){
-
-}
+Sphere::Sphere(Point center, Scalar radius) : _center(center), _radius(radius){}
 
 bool Sphere::intersect(const Ray& ray, Point& out) const {
     // the equation to solve is : f(t) = (t**2)*(||d||)**2 -2*t*<d, OA> + ||OA||**2 - R**2 = 0
     Scalar scalarProduct = ray.direction.dot(fromTo(ray.origin, _center));
     Scalar squaredNormD = ray.direction.squaredNorm();
     Scalar reduced_discrim = (scalarProduct*scalarProduct - squaredNormD*(  fromTo(ray.origin, _center).squaredNorm() - _radius*_radius   ) );
-    
+
     if (reduced_discrim < 0)
       return false;
 
@@ -25,14 +23,9 @@ bool Sphere::intersect(const Ray& ray, Point& out) const {
     if (t1 < 0 and t2 < 0)
       return false;
 
-    if (t1 < 0 and t2 >= 0)
+    if (t1 < 0 || t1 > t2)
       out = ray.getPoint(t2);
-    else if (t1 >= 0 and t2  < 0)
-      out = ray.getPoint(t1);
-      //here, we are sure that t1 >=0 and t2 >=0
-    else if(t1 > t2)
-      out = ray.getPoint(t2);
-    else
+    else if (t2  < 0 || t2 < t1)
       out = ray.getPoint(t1);
 
     return true;
