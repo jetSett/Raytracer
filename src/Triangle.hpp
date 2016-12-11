0b62ee42cdc4c002f707e5e140bbca13e3c2dff2
Algue-Rythme <algue-rythme@orange.fr>
2016-12-11 19:05:21 +0100

    Typo.

--- src/Triangle.hpp
+++ src/Triangle.hpp
@@ -11,7 +11,7 @@ public:
     /// \arg the vertices of our triangle
     Triangle(Point v1, Point v2, Point v3);
 
-    virtual Scalar intersection(const Ray&) const;
+    virtual Scalar intersection(const Ray&) const override;
 private:
     Point _v1, _v2, _v3;
 };
