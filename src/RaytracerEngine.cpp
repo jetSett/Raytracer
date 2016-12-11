11e3919699770c09c764a7bb71d8e7a22d73e0d4
Algue-Rythme <algue-rythme@orange.fr>
2016-12-11 19:01:53 +0100

    Implement void RaytracerEngine::updateScreen(uint32_t backgroundColor, const Camera& camera)

--- src/RaytracerEngine.cpp
+++ src/RaytracerEngine.cpp
@@ -2,7 +2,7 @@
 
 #include <vector>
 
-RaytracerEngine::RaytracerEngine(const Collection& scene, SceneDisplayer& sceneDisplayer):
+RaytracerEngine::RaytracerEngine(const Collection& collection, SceneDisplayer& sceneDisplayer):
     _collection(collection), _sceneDisplayer(sceneDisplayer) {}
 
 void RaytracerEngine::updateScreen(uint32_t backgroundColor, const Camera& camera) {
@@ -10,7 +10,15 @@ void RaytracerEngine::updateScreen(uint32_t backgroundColor, const Camera& camer
     for (unsigned int line = 0; line < _sceneDisplayer.height(); ++line) {
         for (unsigned int column = 0; column < _sceneDisplayer.width(); ++column) {
             Ray ray = camera.getRay(line, column);
-            // Scalar t = ;
+            Scalar t = _collection.intersect(ray);
+            if (t == no_intersection)
+                continue ;
+
+            t /= Scalar(1000);
+            uint32_t color = 255 - uint32_t(t*255);
+            color <<= 24;
+
+            screen[line*_sceneDisplayer.width() + column] = color;
         }
     }
     _sceneDisplayer.update(screen);
