74f9353c7dbcaa84f8c33dbb298b8b4cac67644d
Algue-Rythme <algue-rythme@orange.fr>
2016-12-11 17:36:11 +0100

    Implement Camera::getRay(unsigned int, unsigned int).

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,1 +1,20 @@
 #include "Camera.hpp"
+
+static Scalar farfarAway = Scalar(1000);
+
+Camera::Camera(const Point& position, const Vect3& direction, Scalar gammaWidth, Scalar gammaHeight):
+    _position(position), _direction(direction), _width(width), _height(height), _gammaWidth(gammaWidth), _gammaHeight(gammaHeight),
+{}
+
+Ray Camera::getRay(unsigned int linePixel, unsigned int columnPixel) const {
+    Scalar line = Scalar(linePixel) - Scalar(height)/2;
+    Scalar column = Scalar(columnPixel) - Scalar(width)/2;
+
+    Point rayOrigin(_position);
+    rayOrigin += Vect3(line, column, Scalar(0));
+
+    Point rayTarget(_position);
+    rayTarget += Vect3(line*gammaWidth, column*gammaHeight, farfarAway));
+
+    return Ray(rayOrigin, fromTo(rayOrigin, rayDirection));
+}
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -13,16 +13,29 @@ public:
     /// \brief construct a new Camera
     /// param position position of camera
     /// param direction direction of view, focal plan is normal to it
+    /// param width width in pixels
+    /// param height height in pixels
     /// param gammaWidth ratio along width axis
     /// param gammaHeight ratio along height axis
-    Camera(const Point& point, const Vect3& direction, Scalar gammaWidth, Scalar gammaHeight);
+    Camera(
+        const Point& position, const Vect3& direction,
+        size_t width, size_t height,
+        Scalar gammaWidth, Scalar gammaHeight);
 
-    Ray getRay(unsigned int, unsigned int);
+    /// \fn Ray getRay(unsigned int line, unsigned int column)
+    /// \brief return Ray to enlight the pixel (line, column)
+    /// param line line of pixel
+    /// param column column of pixel
+    /// WARNING : direction must have the forme (0, 0, z)
+    /// TODO : fix getRay for other directions
+    Ray getRay(unsigned int line, unsigned int column) const;
 
 private:
 
     Point _position;
     Vect3 _normal;
+    size_t _width;
+    size_t _height;
     Scalar _gammaWidth;
     Scalar _gammaHeight;
 };
