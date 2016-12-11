3763a10204bb2494aa4b6c63db217acb23315020
Algue-Rythme <algue-rythme@orange.fr>
2016-12-11 19:41:54 +0100

    Correct compilation's erors

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,28 @@
 #include <SFML/Graphics.hpp>
 
 #include "SceneDisplayer.hpp"
+#include "RaytracerEngine.hpp"
+#include "ShapeManager.hpp"
+#include "Sphere.hpp"
 
 int main(int, char * []) {
-
     const unsigned int width = 512;
     const unsigned int height = 256;
 
+    // RaytracerEngine
+
+    ShapeManager& shapeManager = ShapeManager::getInstance();
+    IShape* bigBall = shapeManager.createShape("Big ball", new Sphere(absolut_origin+Vect3(0, 0, 500), 250));
+    Collection collection;
+    collection.addShape(bigBall);
+
     SceneDisplayer displayer(width, height);
+    RaytracerEngine engine(collection, displayer);
+
+    Camera camera(absolut_origin, Vect3(0., 0., 1.), width, height, 1., 1.);
+    engine.updateScreen(0xFFF0, camera);
+    // Render Loop
+
     sf::RenderWindow window(sf::VideoMode(width, height), "Raytracer 1.0");
 
     while (window.isOpen()) {
