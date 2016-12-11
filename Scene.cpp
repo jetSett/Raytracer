dcff1a81b400664d3a2cc95fd62146da6a40065b
Algue-Rythme <algue-rythme@orange.fr>
2016-12-11 16:10:00 +0100

    Code refactoring.

--- Scene.cpp
+++ Scene.cpp
@@ -4,13 +4,10 @@ Scalar Scene::intersect(const Ray& ray) const {
     Scalar tMax = no_intersection;
     for (const auto& shape : _shapes) {
         Scalar t = shape->intersect(ray);
-        if (t != no_intersection) {
-            if (tMax == no_intersection){
+        if (t == no_intersection)
+            continue ;
+        if (tMax == no_intersection || t < tMax)
                 tMax = t;
-            }else if (t < tMax) {
-                tMax = t;
-            }
-        }
     }
     return tMax;
 }
