eff15c17d2b60fa4ee38bbc72c3f73bc894d3b29
jetSett <joel.felderhoff@ens-lyon.fr>
2016-12-12 15:14:10 +0100

    Correcting typo

--- src/tests/test1.cpp
+++ src/tests/test1.cpp
@@ -17,11 +17,7 @@ void test1(){
     RaytracerEngine engine(collection, displayer);
 
     Camera camera(absolut_origin, Vect3(0., 0., 1.), width, height, 1., 1.);
-<<<<<<< HEAD:src/tests/test1.cpp
     engine.updateScreen(0x00000000, camera);
-=======
-    engine.updateScreen(0xFFFFFFFF, camera);
->>>>>>> d2e85a92f8d84e8ff22231c65d8298ee8780fa6c:src/main.cpp
     // Render Loop
 
     sf::RenderWindow window(sf::VideoMode(width, height), "Raytracer 1.0");
