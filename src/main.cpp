cfa34dac1737e7a8bca1cba02d3c46d08233559b
jetSett <joel.felderhoff@ens-lyon.fr>
2016-12-12 14:17:38 +0100

    Changement dans la fonction main pour ne pas cramer le GPU...

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "RaytracerEngine.hpp"
 #include "ShapeManager.hpp"
 #include "Sphere.hpp"
+#include "Plane.hpp"
 
 int main(int, char * []) {
     const unsigned int width = 1024;
@@ -14,9 +15,8 @@ int main(int, char * []) {
     // RaytracerEngine
 
     ShapeManager& shapeManager = ShapeManager::getInstance();
-    IShape* bigBall = shapeManager.createShape("Big ball", new Sphere(absolut_origin+Vect3(0, 0, 500), 250));
     Collection collection;
-    collection.addShape(bigBall);
+    collection.addShape(shapeManager.createShape("Big ball", new Sphere(absolut_origin+Vect3(0, 0, 500), 250)));
 
     SceneDisplayer displayer(width, height);
     RaytracerEngine engine(collection, displayer);
@@ -29,7 +29,7 @@ int main(int, char * []) {
 
     while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        if(window.waitEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
