ee03af053dc023b66ac2deebcd936d02157bd7ab
Algue <algue-rythme@orange.fr>
2016-12-23 18:22:59 +0100

    Achieve generalization of ResourceManager and update doc.

--- src/ResourceManager.hpp
+++ src/ResourceManager.hpp
@@ -5,48 +5,50 @@
 #include <unordered_map>
 #include <string>
 #include <iostream>
+#include <typeinfo>
 
-/// \class ShapeManager
-/// \brief Manage the creation, deletion, access to all shape
-/// \note Every shape created must be created with the instance of ShapeManager
+/// \class ResourceManager
+/// \brief Manage the creation, deletion, access to all resource
+/// \note Every resource created must be created with the instance of ResourceManager
 template<class T>
 class ResourceManager {
 public:
 
-    /// \fn getInstance
-    /// \brief an implementation of pattern singleton : you have to call this function in order to use the ShapeManager
+    ResourceManager<T>(const ResourceManager<T>&) = delete;
+    ResourceManager<T>(ResourceManager<T>&&) = delete;
+    ResourceManager<T>& operator=(const ResourceManager<T>&) = delete;
+
+    /// \fn getInstanceResourceManager<T>&
+    /// \brief an implementation of pattern singleton : you have to call this function in order to use the ResourceManager
     static ResourceManager<T>& getInstance() {
         return _instance;
     }
 
     ~ResourceManager<T>() {
-        std::cout << "Destructing ResourceManager" << std::endl;
-        for(auto a : _resourceList) {
-            delete a.second;
-        }
+        std::cout << "Destructing ResourceManager of type : " << typeid(T).name() << std::endl;
     }
 
-    /// \fn createShape
-    /// \brief declare and create a shape
-    /// \arg name the name associated to your shape
-    /// \arg shape the newly created shape
-    /// \example std::shared_ptr<IShape> s = ShapeManager::getInstance().createShape("foo", new Sphere(Vect3(0., 0., 0.), 1.));
-    T* createShape(const std::string& name, T* shape){
-        if(_resourceList.find(name) != _resourceList.end()){
-            delete _resourceList[name];
-        }
-        _resourceList[name] = shape;
-        return _resourceList[name];
+    /// \fn createResource
+    /// \brief declare and create a resource
+    /// \arg name the name associated to your resource
+    /// \arg shape the newly created resource
+    /// \example IShape* s = ResourceManager<IShape>::getInstance().createResource("foo", new Sphere(Vect3(0., 0., 0.), 1.));
+    T* createResource(const std::string& name, T* resource){
+        _resources.erase(name);
+        _resources[name] = resource;
+        return _resources[name].get();
     }
 
 private:
     ResourceManager<T>() {
-        std::cout << "Creating ResourceManager" << std::endl;
+        std::cout << "Creating ResourceManager of type : " << typeid(T).name() << std::endl;
     }
 
-    static ResourceManager<T> _instance();
+    static ResourceManager<T> _instance;
 
-    std::unordered_map<std::string, T* > _resourceList;
+    std::unordered_map<std::string, std::unique_ptr<T>> _resources;
 };
 
+template<class T>
+ResourceManager<T> ResourceManager<T>::_instance;
 #endif
