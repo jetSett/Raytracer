990f4c59630b11d2af1552a5a65c50512fd9064e
Algue <algue-rythme@orange.fr>
2016-12-23 17:47:01 +0100

    Rename files.

--- src/ResourceManager.hpp
+++ src/ResourceManager.hpp
@@ -1,10 +1,11 @@
 #ifndef SHAPEMANAGER_HPP
 #define SHAPEMANAGER_HPP
 
-
 #include <memory>
 #include <unordered_map>
 #include <string>
+#include <iostream>
+
 #include "IShape.hpp"
 
 // DEPRECATED
@@ -17,8 +18,16 @@ public:
 
     /// \fn getInstance
     /// \brief an implementation of pattern singleton : you have to call this function in order to use the ShapeManager
-    static ShapeManager& getInstance();
-    ~ShapeManager();
+    static ShapeManager& getInstance() {
+        return _instance;
+    }
+
+    ~ShapeManager() {
+        std::cout << "Destructing ShapeManager" << std::endl;
+        for(auto a : _shapeList) {
+            delete a.second;
+        }
+    }
 
 
     /// \fn createShape
@@ -26,11 +35,20 @@ public:
     /// \arg name the name associated to your shape
     /// \arg shape the newly created shape
     /// \example std::shared_ptr<IShape> s = ShapeManager::getInstance().createShape("foo", new Sphere(Vect3(0., 0., 0.), 1.));
-    IShape* createShape(const std::string& name, IShape* shape);
-private:
-    ShapeManager();
+    IShape* createShape(const std::string& name, IShape* shape){
+        if(_shapeList.find(name) != _shapeList.end()){
+            delete _shapeList[name];
+        }
+        _shapeList[name] = shape;
+        return _shapeList[name];
+    }
 
-    static ShapeManager _instance;
+private:
+    ShapeManager() {
+        std::cout << "Creating ShapeManager" << std::endl;
+    }
+
+    static ShapeManager _instance = ShapeManager();
 
     std::unordered_map<std::string, IShape* > _shapeList;
 };
